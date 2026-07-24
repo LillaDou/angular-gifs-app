2592ab75b59d014b13ba5b9ca206400ce498f69d
Lilla Doughty <lilla.e.j.du@gmail.com>
2026-07-24 09:32:06 +0200

    Environments y path alias

--- src/app/gifs/components/side-menu/side-menu-header/side-menu-header.ts
+++ src/app/gifs/components/side-menu/side-menu-header/side-menu-header.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
+import { environment } from '@environments/environment';
+// import { environment } from '../../../../../environments/environment';
+//* Hemos creado un path alias en el tsconfig.json. Con eso, lo que conseguimos es que no sea tan engorrosa
+//* la importación
 
 @Component({
   selector: 'gifs-side-menu-header',
   imports: [],
   templateUrl: './side-menu-header.html',
 })
-export class SideMenuHeader {}
+export class SideMenuHeader {
+
+  envs = environment;
+
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,15 @@
+export const environment = {
+
+    production: true,
+    companyName: 'Gifs',
+    companyName2: 'App',
+    companySlogan: 'Maneja tus gifs',
+
+    //ApiKeys
+    //URLs
+
+};
+
+//? Los objetos de environments tienen dos propiedades: 
+//? - Environment de desarrollo (environment.development)
+//? - Environment de producción

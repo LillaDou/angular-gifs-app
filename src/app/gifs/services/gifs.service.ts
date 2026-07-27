bb52ca0784596fbcdcaa8f894f6fa37d6283ab56
Lilla Doughty <lilla.e.j.du@gmail.com>
2026-07-27 12:30:57 +0200

    Diseño de la página de search page + creación de un método para el http.get para la misma página

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -44,6 +44,22 @@ export class GifService {
         } );
 
     }
+
+
+    searchGifs( query: string ) {
+        this.http.get<GiphyResponse>( `${ environment.giphyUrl }/gifs/search`, {
+            params: {
+                api_key: environment.giphyApiKey,
+                limit: 20,
+                q: query,
+            },
+        } ).subscribe( (resp) => {
+            const gifs = GifMapper.mapGiphyItemsToGifArray(resp.data);
+
+            console.log({search: gifs});
+        } )
+
+    }
 }
 
 // El httpClient nos permite hacer la petición get, put, delete, patch... 
